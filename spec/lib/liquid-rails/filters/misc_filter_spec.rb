1c0e18672ead3662815fecedd831698a0985ef51
Scott Treppa <streppa@entertainment.com>
2017-06-26 16:22:13 -0400

    Updated to accommodate changes to Liquid::Variable#new

--- spec/lib/liquid-rails/filters/misc_filter_spec.rb
+++ spec/lib/liquid-rails/filters/misc_filter_spec.rb
@@ -5,21 +5,23 @@ module Liquid
     describe MiscFilter do
       let(:context) { ::Liquid::Context.new }
 
+      options = ParseContext.new
+
       context '#index' do
         it 'returns value at the specified index' do
           context['array'] = [1, 2, 3]
-          expect(::Liquid::Variable.new("array | index: 0").render(context)).to eq(1)
+          expect(::Liquid::Variable.new("array | index: 0", options).render(context)).to eq(1)
         end
 
         it 'returns nil when outside range' do
           context['array'] = [1, 2, 3]
-          expect(::Liquid::Variable.new("array | index: 5").render(context)).to eq(nil)
+          expect(::Liquid::Variable.new("array | index: 5", options).render(context)).to eq(nil)
         end
       end
 
       it '#jsonify' do
         context['listing'] = { name: 'Listing A' }
-        expect(::Liquid::Variable.new("listing | jsonify").render(context)).to eq(%|{"name":"Listing A"}|)
+        expect(::Liquid::Variable.new("listing | jsonify", options).render(context)).to eq(%|{"name":"Listing A"}|)
       end
     end
   end
--- spec/lib/liquid-rails/filters/translate_filter_spec.rb
+++ spec/lib/liquid-rails/filters/translate_filter_spec.rb
@@ -5,24 +5,26 @@ module Liquid
     describe TranslateFilter do
       let(:context) { ::Liquid::Context.new }
 
+      options = ParseContext.new
+
       before do
         context.registers[:view] = ActionView::Base.new
       end
 
       it 'translate with default locale' do
-        expect(::Liquid::Variable.new("'welcome' | translate").render(context)).to eq('Welcome everyone!')
+        expect(::Liquid::Variable.new("'welcome' | translate", options).render(context)).to eq('Welcome everyone!')
       end
 
       it 'translate with specified locale' do
-        expect(::Liquid::Variable.new("'welcome' | translate: locale: 'km'").render(context)).to eq('សូមស្វាគមន៍')
+        expect(::Liquid::Variable.new("'welcome' | translate: locale: 'km'", options).render(context)).to eq('សូមស្វាគមន៍')
       end
 
       it 'translate with scope' do
-        expect(::Liquid::Variable.new("'home' | translate: locale: 'km', scope: 'links'").render(context)).to eq('ទំព័រដើម')
+        expect(::Liquid::Variable.new("'home' | translate: locale: 'km', scope: 'links'", options).render(context)).to eq('ទំព័រដើម')
       end
 
       it 'translate with interpolation' do
-        expect(::Liquid::Variable.new("'welcome_name' | translate: locale: 'en', name: 'Jeremy'").render(context)).to eq('Welcome, Jeremy')
+        expect(::Liquid::Variable.new("'welcome_name' | translate: locale: 'en', name: 'Jeremy'", options).render(context)).to eq('Welcome, Jeremy')
       end
     end
   end
